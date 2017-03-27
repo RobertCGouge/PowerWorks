e6f4547b0ca223a074330ef376fcec2e51e96c44
Robert Gouge <rgouge@askcts.com>
2017-03-27 09:24:17 -0500

    Write additional Pester test for TCP port number checking

--- Tests/Test-Port.Tests.ps1
+++ Tests/Test-Port.Tests.ps1
@@ -14,4 +14,7 @@ Describe "Test-Port" {
     It "Should throw an error on an invalid TCP port number" {
         {Test-port -Computer . -Port 65536} | should throw
     }
+    It "Should not throw on a valid TCP port number" {
+        Test-port -Computer . -Port 25 | should not throw
+    }
 }
