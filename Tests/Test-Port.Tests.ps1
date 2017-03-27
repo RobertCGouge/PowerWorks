f02af9dffc677f39b605005dedf00bd443f8d6e2
Robert Gouge <robert.c.gouge@gmail.com>
2017-03-27 13:44:49 -0500

    deploy Add new test case. Test deployment flag

--- Tests/Test-Port.Tests.ps1
+++ Tests/Test-Port.Tests.ps1
@@ -10,10 +10,14 @@ import-module $module
 
 
 
-Describe "Test-Port" {
+Describe "Test-Port -Port parameter" {
     It "Should throw an error on an invalid TCP port number" {
         {Test-port -Computer . -Port 65536} | should throw
     }
+    It "Should throw an error when a string is passed" {
+        {Test-port -Computer . -Port string}    
+    }
+
     It "Should not throw on a valid TCP port number" {
         {Test-port -Computer . -Port 25} | should not throw
     }
