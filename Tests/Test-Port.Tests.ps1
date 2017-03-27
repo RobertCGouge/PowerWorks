8e8819c80ca8015d059072774dcd5c751c2eceb2
Robert Gouge <rgouge@askcts.com>
2017-03-27 09:33:39 -0500

    Fix Pester test

--- Tests/Test-Port.Tests.ps1
+++ Tests/Test-Port.Tests.ps1
@@ -15,6 +15,6 @@ Describe "Test-Port" {
         {Test-port -Computer . -Port 65536} | should throw
     }
     It "Should not throw on a valid TCP port number" {
-        Test-port -Computer . -Port 25 | should not throw
+        {Test-port -Computer . -Port 25} | should not throw
     }
 }
