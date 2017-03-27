6abdea8ccc36bf79eb3b563b89050a91696e7b26
RobertCGouge <robert.c.gouge@gmail.com>
2017-03-26 21:31:38 -0500

    Start work on Test-Port.  Create initial tests

---
+++ PowerWorks/Public/Test-Port.ps1
@@ -0,0 +1,41 @@
+﻿#requires -Version 2
+<#
+        .Synopsis
+        This cmdlet will test a given TCP port till the port is open
+        .DESCRIPTION
+        This cmdlet will continously test a given TCP port untill the cmdlet is halted or a connection to the given port is established.  This is useful for testing connectivity while configuring firewall rules.
+        .EXAMPLE
+        Example of how to use this cmdlet
+        .EXAMPLE
+        Another example of how to use this cmdlet
+#>
+function Test-Port
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        # This is the remote machine you want to test connectivity to on the given port.
+        [Parameter(Mandatory = $true,
+                ValueFromPipelineByPropertyName = $true,
+                Position = 0)]
+        $Computer,
+
+        # This is the port you wish to test connectivity to.
+        [Parameter(Mandatory = $true,
+                Position = 1)]
+        [ValidateRange(1,65535)]
+        [int]
+        $Port
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+    }
+    End
+    {
+    }
+}
---
+++ Tests/Test-Port.Tests.ps1
@@ -0,0 +1,10 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+
+. ..\PowerWorks\Public\$sut
+
+Describe "Test-Port" {
+    It "Should throw an error on an invalid TCP port number" {
+        {Test-port -Computer . -Port 65536} | should throw
+    }
+}
