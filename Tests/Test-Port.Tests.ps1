9d5e08629c474f3b3d52e61fddb087ba0ab6f985
Robert Gouge <robert.c.gouge@gmail.com>
2017-03-28 15:26:53 -0500

    deploy More fixes for tests

--- Tests/Test-Port.Tests.ps1
+++ Tests/Test-Port.Tests.ps1
@@ -7,27 +7,27 @@ $module = (Get-ChildItem -Path $root.FullName -Recurse -Filter '*.psm1').FullNam
 import-module $module
 
 
-$ip = Resolve-DnsName -Name localhost | Where-Object -FilterScript {$_.Type -like 'AAAA'}
+$ip = Resolve-DnsName -Name . | Where-Object -FilterScript {$_.Type -like 'AAAA'}
 
 Describe "Test-Port -Port parameter" {
     It "Should throw an error on an invalid TCP port number" {
-        {Test-port -Computer localhost -Port 65536} | should throw
+        {Test-port -Computer . -Port 65536} | should throw
     }
     It "Should throw an error when a string is passed" {
-        {Test-port -Computer localhost -Port string | should throw}    
+        {Test-port -Computer . -Port string | should throw}    
     }
 
     It "Should not throw on a valid TCP port number" {
-        {Test-port -Computer localhost -Port 80} | should not throw
+        {Test-port -Computer . -Port 53} | should not throw
     }
 }
 
 Describe "Test-Port -Computer parameter"{
     It "Should accecpt a string" {
-        {Test-port -Computer localhost -Port 80 | should not throw}
+        {Test-port -Computer . -Port 53 | should not throw}
     }
     It "Should throw when passed a name that is unabled to be resolved"{
-        {Test-Port -Computer notfound -Port 80 | should throw "$_ could not be resolved, please try another computer name."}
+        {Test-Port -Computer notfound -Port 53 | should throw "$_ could not be resolved, please try another computer name."}
     }
         It "Should throw when passed an ip address" {
         {Test-port -Computer 8.8.8.8 -Port 53 | should throw "$_ is an IP address, please try again with -IPv4"}
@@ -36,7 +36,7 @@ Describe "Test-Port -Computer parameter"{
 
 Describe "Test-Port -IPv4"{
     It "Should throw when an invalid IPv4 is passed" {
-        {test-port -IP 555.555.555.555 -port 80 | should throw "$_ is not a valid IPv4 Address"}
+        {test-port -IP 555.555.555.555 -port 53 | should throw "$_ is not a valid IPv4 Address"}
     }
     It "Should not throw when a valid IPv4 is passed" {
         {test-port -IP 8.8.8.8 -port 53 | should not throw}
