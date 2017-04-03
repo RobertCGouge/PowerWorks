564b16be193200aab0136d22004dd73a0baed9e1
RobertCGouge <robert.c.gouge@gmail.com>
2017-04-02 20:05:17 -0500

    Start work on Get-SCVirtualMachineAge

---
+++ PowerWorks/Public/Get-SCVirtualMachineAge.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.Synopsis
+   This function will return how old a virtual machine is and how long it has been off.
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-SCVirtualMachineAge
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([VirtualMachine])]
+    Param
+    (
+        # This is System Center Virtual Machine Manager you want to query for Virtual Machines
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0,
+                   ParameterSet='SCVMM')]
+        $VMMServer
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+    }
+    End
+    {
+    }
+}
